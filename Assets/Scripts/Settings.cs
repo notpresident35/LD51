bce139236c1d080a25c6ed121466b06ef5514a99
notpresident35 <notpresident35@gmail.com>
2022-10-01 15:20:53 -0700

    Settings started

--- Assets/Scripts/Settings.cs
+++ Assets/Scripts/Settings.cs
@@ -1,8 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Settings : MonoBehaviour
 {
-    float val = PlayerPrefHandler.GetFloat (Statics.AudioMasterVolumePPD);
+    public Slider MasterVolumeSlider;
+    public Slider SFXVolumeSlider;
+    public Slider MusicVolumeSlider;
+
+    public void Start () {
+        Load ();
+    }
+
+    public void Load () {
+        MasterVolumeSlider.value = PlayerPrefHandler.GetFloat (Statics.AudioMasterVolumePPD);
+        SFXVolumeSlider.value = PlayerPrefHandler.GetFloat (Statics.AudioSFXVolumePPD);
+        MusicVolumeSlider.value = PlayerPrefHandler.GetFloat (Statics.AudioMusicVolumePPD);
+
+        // TODO: Player inputs!
+    }
+
+    public void Save () {
+        PlayerPrefHandler.SetFloat (Statics.AudioMasterVolumePPD, MasterVolumeSlider.value);
+        PlayerPrefHandler.SetFloat (Statics.AudioSFXVolumePPD, SFXVolumeSlider.value);
+        PlayerPrefHandler.SetFloat (Statics.AudioMusicVolumePPD, MusicVolumeSlider.value);
+
+        // TODO: Player inputs!
+    }
 }
