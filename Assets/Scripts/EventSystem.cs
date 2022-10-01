0c708be9137d900e83d5e311f4ca3d5f09f887e5
ElerosVecchio <samowenhansen@gmail.com>
2022-10-01 15:42:10 -0700

    update events, goal. started vfx
    added new event to manager, added invoke to goal. started vfx manager and created ball explosion particle system

--- Assets/Scripts/EventSystem.cs
+++ Assets/Scripts/EventSystem.cs
@@ -7,5 +7,18 @@ public class EventSystem : MonoBehaviour {
 
     public UnityEvent<int, Vector3> goalHit;
     public UnityEvent<Vector3> paddleHit;
+    public UnityEvent settingsSet;
+
+    public static EventSystem Instance;
+
+    private void Start() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+    }
 
 }
--- Assets/Scripts/VFXManager.cs
+++ Assets/Scripts/VFXManager.cs
@@ -2,7 +2,26 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class VFXManager : MonoBehaviour
-{
-    
+public class VFXManager : MonoBehaviour {
+
+    public GameObject ballExplosionPS;
+
+    public static VFXManager Instance;
+
+    private void Start() {
+        if (Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        } else {
+            Destroy(gameObject);
+            return;
+        }
+
+        EventSystem.Instance.goalHit.AddListener(createBallExplosion);
+    }
+
+    private void createBallExplosion(int team, Vector3 coords) {
+        Instantiate(ballExplosionPS, coords, Quaternion.identity);
+    }
+
 }
