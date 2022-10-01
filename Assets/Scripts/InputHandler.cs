ec9c89cc8bfd07f073b863a6132fd56a37492fdd
notpresident35 <notpresident35@gmail.com>
2022-10-01 14:31:01 -0700

    Input handler/player prefs

--- Assets/Scripts/InputHandler.cs
+++ Assets/Scripts/InputHandler.cs
@@ -3,11 +3,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using static PlayerPrefHandler;
+/*
+[Serializable]
+public class InputKeycodePlayerPrefData : PlayerPrefData<KeyCode> {
+    public string HandlerNamePrefix;
+    public InputKeycodePlayerPrefData (string name, KeyCode defaultVal) : base (name, defaultVal) {
+    }
+}*/
 
 [Serializable]
 public class InputHandler : MonoBehaviour
 {
+    //[SerializeField]
+    //private string InputHandlerName;
     [Tooltip ("This should be stored/read as a list of inputs")]
     [SerializeField]
-    private static List<StringPlayerPrefData> InputKeycodePPDs;
+    private List<KeycodePlayerPrefData> InputKeycodePPDs;
+
+    public KeyCode GetKeycodeForInput (string inputName) {
+        foreach (KeycodePlayerPrefData p in InputKeycodePPDs) {
+            if (p.Name == inputName) {
+                return GetKeycode (p);
+            }
+        }
+        Debug.LogError ("Failed to get keycode!");
+        return KeyCode.Home;
+    }
 }
