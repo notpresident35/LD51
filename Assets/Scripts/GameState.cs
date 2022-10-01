8aa487ca919b36918f48ac1c24d839e7f7ce5eea
Rose Zhang <rosewenzhang@gmail.com>
2022-10-01 15:25:10 -0700

    implemented GameState.cs

--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class GameState : MonoBehaviour
 {
+    // false, false: between rounds
+    // false, true : game just ended
+    // true,  false: game is running
+    // true,  true : never
+    public bool IsBallActive;
+    public bool IsGameComplete;
+
+    // paused is independent of ball active and game complete. When unpaused, the game's behavior
+    // determined by isBallActive and isGameComplete.
+    public bool paused;
     
+    public GameState(bool isBallActive, bool isGameComplete)
+    {
+        this.isBallActive = isBallActive;
+        this.isGameComplete = isGameComplete;
+    }
 }
