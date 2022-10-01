ea61f812c83aea4be4be756fdf1225cf8d2e8ae7
Rose Zhang <rosewenzhang@gmail.com>
2022-10-01 14:16:19 -0700

    implemented audiomanager

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -2,7 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
-    
+    public AudioSource audioSource;
+
+    // fileName is the name of a file in the SFX directory
+    public static void PlaySound(string fileName)
+    {
+        string audioDirPrefix = "SFX/";
+        AudioClip clip = (AudioClip)Resources.Load(audioDirPrefix + fileName);
+        AudioSource.PlayClipAtPoint(clip, new Vector3(0, 0, 0));
+    }
 }
