3c1fb6cfdc072b709859fd2aca33317468cd58c4
notpresident35 <notpresident35@gmail.com>
2022-10-01 15:29:38 -0700

    TeamManager complete

--- Assets/Scripts/TeamManager.cs
+++ Assets/Scripts/TeamManager.cs
@@ -4,5 +4,23 @@ using UnityEngine;
 
 public class TeamManager : MonoBehaviour
 {
-    
+    public class Team {
+        public float Score;
+        public List<Paddle> Paddles;
+        public List<Goal> Goals;
+    }
+
+    public List<Team> Teams = new List<Team>();
+
+    public void RegisterPaddle (Paddle paddle, int teamID) {
+        Teams [teamID].Paddles.Add (paddle);
+    }
+
+    public void RegisterGoal (Goal goal, int teamID) {
+        Teams [teamID].Goals.Add (goal);
+    }
+
+    public void ScorePoint (int teamID) {
+        Teams [teamID].Score++;
+    }
 }
