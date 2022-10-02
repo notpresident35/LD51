bea86fa5b7b4a04195212b8bc646a5e162b09dbf
Rose Zhang <rosewenzhang@gmail.com>
2022-10-02 11:53:17 -0700

    added lerping to camera controller

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -8,18 +8,24 @@ public class CameraController : MonoBehaviour
     public float MinZoomSize;
     public Vector2 EdgePadding;
 
+    // TODO: move this magic number to Statics.cs eventually
+    [SerializeField]
+    private float smoothFactor = 0.99f;
+
+    private Bounds lastBounds;
+
     Camera cam;
 
-    private void Awake () {
+    private void Awake() {
         cam = GetComponentInChildren<Camera> ();
     }
 
     void Update()
     {
-        FocusOnBounds (GetTargetedBounds ());
+        FocusOnBounds(GetTargetedBounds());
     }
 
-    Bounds GetTargetedBounds () {
+    Bounds GetTargetedBounds() {
         // Focus on center position with minimum zoom
         Vector3 centerPos = Vector3.zero;
         foreach (CameraTracker trackedObj in CameraTracker.Trackers) {
@@ -34,15 +40,23 @@ public class CameraController : MonoBehaviour
         }
         return bounds;
     }
+    
+    
+    Vector3 Interpolate(Vector3 a, Vector3 b)
+    {
+        return Vector3.Lerp(a, b, smoothFactor);
+    }
 
     void FocusOnBounds (Bounds bounds) {
         bounds.Expand (new Vector2 (-bounds.extents.x * cam.aspect * WidthCompression, 0));
         bounds.Expand (EdgePadding);
+        bounds = new Bounds(Interpolate(lastBounds.center, bounds.center), Interpolate(lastBounds.size, bounds.size));
 
         // Position camera
         transform.position = bounds.center;
 
         // Size to focus on bounds
         cam.orthographicSize = Mathf.Max (bounds.extents.x, bounds.extents.y);
+        lastBounds = bounds;
     }
 }
