b0bf23adcd4597dca628ee679986d6a40acf277d
notpresident35 <notpresident35@gmail.com>
2022-10-02 23:50:13 -0700

    Completed background

--- Assets/Scripts/Parallax.cs
+++ Assets/Scripts/Parallax.cs
@@ -4,15 +4,18 @@ using UnityEngine;
 
 public class Parallax : MonoBehaviour
 {
+    public Vector3 grid2Offset = new Vector3 (0.2f, 0.5f, 0);
     public Transform cam;
+    public Camera curCam;
     public Transform grid1;
     public Transform grid2;
 
-    public float grid1dist;
-    public float grid2dist;
+    public float grid1parallaxing;
+    public float grid2parallaxing;
+    public float camBaselineZoom;
 
     private void Update () {
-        grid1.position = cam.position / grid1dist;
-        grid2.position = cam.position / grid2dist;
+        grid1.localPosition = cam.position * -grid1parallaxing;
+        grid2.localPosition = cam.position * -grid2parallaxing + grid2Offset;
     }
 }
