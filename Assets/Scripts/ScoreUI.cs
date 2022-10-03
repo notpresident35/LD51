235eb57c42081b68d117efe44ef381ec5044ce5d
notpresident35 <notpresident35@gmail.com>
2022-10-02 20:23:54 -0700

    Completed scoreUI

--- Assets/Scripts/ScoreUI.cs
+++ Assets/Scripts/ScoreUI.cs
@@ -1,11 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class ScoreUI : MonoBehaviour
 {
-    public List<Text> ScoreTexts;
+    private List<TMP_Text> ScoreTexts;
+
+    private void Awake () {
+        ScoreTexts = GetComponentsInChildren<TMP_Text> ().ToList();        
+    }
 
     void Refresh (int teamID, Vector3 pos) {
         for (int i = 0; i < ScoreTexts.Count; i++) {
@@ -13,11 +19,27 @@ public class ScoreUI : MonoBehaviour
         }
     }
 
+    void EnableUI (int teamID, Vector3 pos) {
+        for (int i = 0; i < ScoreTexts.Count; i++) {
+            ScoreTexts [i].gameObject.SetActive (true);
+        }
+    }
+    
+    void DisableUI () {
+        for (int i = 0; i < ScoreTexts.Count; i++) {
+            ScoreTexts [i].gameObject.SetActive (false);
+        }
+    }
+
     private void OnEnable () {
         SingletonManager.EventSystemInstance.OnGoalHit.AddListener(Refresh);
+        SingletonManager.EventSystemInstance.OnGoalHit.AddListener(EnableUI);
+        SingletonManager.EventSystemInstance.OnRoundBegin.AddListener(DisableUI);
     }
 
     private void OnDisable () {
         SingletonManager.EventSystemInstance.OnGoalHit.RemoveListener (Refresh);
+        SingletonManager.EventSystemInstance.OnGoalHit.RemoveListener (EnableUI);
+        SingletonManager.EventSystemInstance.OnRoundBegin.RemoveListener (DisableUI);
     }
 }
